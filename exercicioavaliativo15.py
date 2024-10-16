874795868a23eca2942e4cc224336f0f4d3b01f4
lorrayneteles <lorrayneteles1305@gmail.com>
2024-10-16 18:38:53 -0300

    Add files via upload

---
+++ exercicioavaliativo15.py
@@ -0,0 +1,9 @@
+#Cálculo de horas trabalhadas
+
+quantidade_horas = int(input("Digite a quantidade de horas trabalhadas:"))
+
+valor_hora = int(input("Digite o valor por hora trabalhado:"))
+
+salario = quantidade_horas * valor_hora
+
+print(f"O valor do salário total é:", salario)
